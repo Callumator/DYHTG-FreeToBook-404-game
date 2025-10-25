c2993d47fb3c98de026feedc21783682c90b9701
callumator <calsmith247@gmail.com>
2025-10-25 18:55:52 +0100

    PlaneText

--- main.js
+++ main.js
@@ -24,9 +24,21 @@ const STATES = [
   GAME_STATES.POST_GUESS
 ];
 
+/**
+ * List of game states the game should tick for.
+ *
+ * @see FreeToBook.render
+ */
+const TICKING_STATES = [
+  GAME_STATES.X_AXIS,
+  GAME_STATES.Y_AXIS,
+];
+
 /** IDs used in the component, saves me fuging up spelling, once again sorry excuse for an enum. */
 const IDS = Object.freeze({
-  BODY: 'free-to-book-body'
+  BODY: 'free-to-book-body',
+  PLANE: 'free-to-book-plane',
+  SUBTEXT: 'free-to-book-subtext'
 });
 
 /** ASCII character code for space */
@@ -35,15 +47,28 @@ const SPACE = 32;
 /** Tick rate for the game, decides at what speed we the entire game runs */
 const TICK_RATE = 100;
 
-/** px for the plane to move every tick */
-const PLANE_SPEED = 100;
+/** % of the map for the plane to move every tick, SHOULD BE LESS THAN 1 */
+const PLANE_SPEED = 0.05;
 
 class FreeToBook extends HTMLElement {
-  #ticker;
-
   #state = GAME_STATES.PRE_START;
 
   #stateCounter = 0;
+
+  /** Reference to the tick method */
+  #ticker;
+
+  /** Element for the body of the component */
+  #body;
+
+  /** Instance for the plane */
+  #plane;
+
+  /** Text to show below the map */
+  #subtext;
+
+  /** Flag to stop the game from ticking */
+  #busy = false;
 
   constructor() {
     super();
@@ -60,9 +85,8 @@ class FreeToBook extends HTMLElement {
    * than the constructor.
    */
   connectedCallback() {
-    this.#render();
+    this.render();
   }
-
 
   /**
    * Called on every tick after `render()`, needs to be tracked using `#ticker` and should be cleaned
@@ -72,79 +96,157 @@ class FreeToBook extends HTMLElement {
    */
   #onTick() {
     switch (this.#state) {
-      case GAME_STATES.PRE_START: {
-        console.log('pre-start');
-        break;
-      }
-
       case GAME_STATES.X_AXIS: {
-        console.log('x-axis');
+        if (!this.#plane.style.left) {
+          this.#plane.style.left = 0;
+        }
+
+        const currentPos = parseInt(this.#plane.style.left);
+        const nextStep = this.#body.offsetWidth * PLANE_SPEED;
+        const direction = this.#plane.style.transform.indexOf("scaleX") != -1 ? 'left' : 'right';
+
+        if (direction === 'right') {
+          if ((currentPos + nextStep) > this.#body.offsetWidth - this.#plane.offsetWidth / 2) {
+            this.#plane.style.transform = `${this.#plane.style.transform} scaleX(-1)`;
+            break;
+          }
+
+          this.#plane.style.left = `${currentPos + nextStep}px`;
+          break;
+        }
+
+        if ((currentPos - nextStep) < 0) {
+          this.#plane.style.transform = this.#plane.style.transform.replace('scaleX(-1)', '');
+          break;
+        }
+
+        this.#plane.style.left = `${currentPos - nextStep}px`;
         break;
       }
 
       case GAME_STATES.Y_AXIS: {
-        console.log('y-axis');
-        break;
-      }
+        if (!this.#plane.style.top) {
+          this.#plane.style.top = 0;
+        }
 
-      case GAME_STATES.POST_GUESS: {
-        console.log('post-guess');
+        const currentPos = parseInt(this.#plane.style.top);
+        const nextStep = this.#body.offsetHeight * PLANE_SPEED;
+        const direction = this.#plane.style.transform.indexOf("rotate(180deg)") != -1 ? 'down' : 'up';
+
+        if (direction === 'up') {
+          if ((currentPos + nextStep) > this.#body.offsetHeight - this.#plane.offsetHeight / 2) {
+            this.#plane.style.transform = `${this.#plane.style.transform} rotate(180deg)`;
+            break;
+          }
+
+          this.#plane.style.top = `${currentPos + nextStep}px`;
+          break;
+        }
+
+        if ((currentPos - nextStep) < 0) {
+          this.#plane.style.transform = this.#plane.style.transform.replace('rotate(180deg)', '');
+          break;
+        }
+
+        this.#plane.style.top = `${currentPos - nextStep}px`;
         break;
       }
     }
   }
 
+  /** Called on `keydown` of `space`, will update `this.#state` and render the updates to the ui. */
   #switchState() {
-    this.#stateCounter++;
-    this.#state = STATES[this.#stateCounter % (STATES.length)];
+    this.#busy = true;
 
-    switch (this.#state) {
+    this.#stateCounter++;
+    const state = STATES[this.#stateCounter % (STATES.length)];
+
+    switch (state) {
       case GAME_STATES.PRE_START: {
-        console.log('pre-start');
+        if (this.#plane) {
+          this.#body.removeChild(this.#plane);
+          this.#plane = undefined;
+        }
+
+        this.#subtext.innerText = 'Press Space to Start!';
         break;
       }
 
       case GAME_STATES.X_AXIS: {
-        console.log('x-axis');
+        this.#plane = document.createElement('img')
+        this.#plane.id = IDS.PLANE;
+        this.#plane.src = '/assets/plane.png';
+        this.#plane.classList.add('plane');
+
+        this.#body.appendChild(this.#plane);
+        this.#subtext.innerText = 'Select X axis';
+
         break;
       }
 
       case GAME_STATES.Y_AXIS: {
-        console.log('y-axis');
+        this.#plane.style.transform = `${this.#plane.style.transform} rotate(90deg)`;
+        this.#subtext.innerText = 'Select Y axis';
         break;
       }
 
       case GAME_STATES.POST_GUESS: {
-        console.log('post-guess');
+        // this bit is hard
+        this.#subtext.innerText = 'You have ____ points!';
+        const x = this.#plane.style.left;
+        const y = this.#plane.style.top;
+
+        console.log(x, y);
+
         break;
       }
     }
+
+    // assign state only after the switch state has been finalised, we don't want to tick for a state before it's actually been initialised
+    this.#state = state;
+    this.#busy = false;
   }
 
   /** Initially render the component and setup listeners */
-  #render() {
-    this.#ticker = setInterval(() => this.#onTick(), TICK_RATE);
-
-    addEventListener('keydown', e => {
-      if (e.keyCode === SPACE) {
-        this.#switchState()
-      }
-    });
+  render() {
+    this.#ticker = setInterval(() => {if (this.#busy === false && TICKING_STATES.includes(this.#state)) this.#onTick()}, TICK_RATE);
+    addEventListener('keydown', e => {if (e.keyCode === SPACE) this.#switchState()});
 
     const shadowRoot = this.attachShadow({mode: 'open'});
     shadowRoot.innerHTML = `
       <style>
         :host {
-          background: grey
+          background: grey;
+          position: relative;
         }
+
         .map {
           width: 100%;
         }
+
+        .plane {
+          width: 10%;
+          position: absolute;
+          left: 0;
+        }
+
+        .subtext {
+          align-items: center;
+          text-align: center;
+          padding: 20px;
+        }
       </style>
+
       <div id=${IDS.BODY}>
-        <img src='/assets/map.png' class='map'>
+        <img src='/assets/map.png' id=${IDS.BODY} class='map'>
       </div>
+
+      <p id=${IDS.SUBTEXT} class='subtext'></p>
     `;
+
+    this.#body = shadowRoot.getElementById(IDS.BODY);
+    this.#subtext = shadowRoot.getElementById(IDS.SUBTEXT);
+    this.#subtext.innerText = 'Press Space to Start!';
   }
 }
 
